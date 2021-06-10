2642df095b8aee4df608b628a5d93c25eb9c821d
hijang <hanil.jang@gmail.com>
2021-06-11 00:33:33 +0900

    * FakeCCTV에 ssl 적용
    * FIXME: 작업 공유를 위해 임시 수정 부분이 포함됨
      - key 경로
      - 빌드 설정 경로

--- LgFaceRecDemoTCP_Jetson_NanoV2/src/sslConnect.cpp
+++ LgFaceRecDemoTCP_Jetson_NanoV2/src/sslConnect.cpp
@@ -1,7 +1,25 @@
 #include <opencv2/highgui/highgui.hpp>
+
+#if  defined(_WIN32) || defined(_WIN64)
+#pragma comment (lib, "Ws2_32.lib")
+#include <Winsock2.h>
+#include <ws2tcpip.h>
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#define  CLOSE_SOCKET closesocket
+#define  SOCKET_FD_TYPE SOCKET
+#define  BAD_SOCKET_FD INVALID_SOCKET
+#else
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+#define  CLOSE_SOCKET close
+#define  SOCKET_FD_TYPE int
+#define  BAD_SOCKET_FD  -1
+#endif
 
 #include "sslConnect.h"
 
@@ -84,7 +102,7 @@ bool SslConnect::loadCertification()
 
 	// set local rootca cert
 	
-	if(!SSL_CTX_load_verify_locations(m_ctx,"/home/hedaesik/work/CppSocketTest/Certificates/rootca.crt", NULL) || 
+	if(!SSL_CTX_load_verify_locations(m_ctx,"../key/rootca.crt", NULL) || 
            !SSL_CTX_set_default_verify_paths(m_ctx)) {
         	fprintf(stderr, "Can't load CA cert\n");
         	ERR_print_errors_fp(stderr);
