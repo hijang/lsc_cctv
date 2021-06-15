75110ce29ca0f47f66d1b5db2b5ae0bc55e6780e
Jinchul Kim <tehloo@gmail.com>
2021-06-15 00:38:16 -0400

    Return code as it gets from binary to let systemd restart the service.

--- cctv.sh
+++ cctv.sh
@@ -6,4 +6,6 @@ WORKING_DIR="$HOME/work/lsc_cctv/LgFaceRecDemoTCP_Jetson_NanoV2/build"
 cd $WORKING_DIR
 ./LgFaceRecDemoTCP_Jetson_NanoV2 $PORT_TO_LISTEN 1
 
-echo "END OF cctv.sh"
+ret=$?
+echo "END OF cctv.sh with return code ${ret}"
+exit ${ret}
