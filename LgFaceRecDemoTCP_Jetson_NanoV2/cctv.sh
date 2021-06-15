4ea614297c8c100e4c341581c9c761f36fab063c
JC KIM <jinchul.kim@lge.com>
2021-06-15 12:36:03 +0900

    cctv.sh is using to start cctv.service by systemd
    To deploy, make symbolic link to /home/cctv/cctv.sh which is invoked by
    cctv.service.

---
+++ LgFaceRecDemoTCP_Jetson_NanoV2/cctv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+PORT_TO_LISTEN="7997"
+WORKING_DIR="$HOME/work/lsc_cctv/LgFaceRecDemoTCP_Jetson_NanoV2/build"
+
+cd $WORKING_DIR
+./LgFaceRecDemoTCP_Jetson_NanoV2 $PORT_TO_LISTEN 1
+
+echo "END OF cctv.sh"
