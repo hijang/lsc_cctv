071b69614c73f1d50c5196a9d7d88c9d2432996d
cctv <cctv@potential4.org>
2021-06-16 05:28:29 -0400

    Remove load in script

--- fk.sh
+++ fk.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-keyctl add user fk "load `cat $HOME/keys/fk.blob`" @u
-keyctl add user fnk "load `cat $HOME/keys/fnk.blob`" @u
+keyctl add user fk "`cat $HOME/keys/fk.blob`" @u
+keyctl add user fnk "`cat $HOME/keys/fnk.blob`" @u
