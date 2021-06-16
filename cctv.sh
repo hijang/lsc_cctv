da1b016975141e3fe044dc6e8f8f96d7d50c5678
hedaesik.kim <hedaesik.kim@lge.com>
2021-06-16 07:25:32 -0400

    Change launch script because service param is changed

--- cctv.sh
+++ cctv.sh
@@ -3,7 +3,7 @@
 WORKING_DIR="$HOME/work/lsc_cctv/LgFaceRecDemoTCP_Jetson_NanoV2/build"
 
 cd $WORKING_DIR
-./LgFaceRecDemoTCP_Jetson_NanoV2 1
+./LgFaceRecDemoTCP_Jetson_NanoV2
 
 ret=$?
 echo "END OF cctv.sh with return code ${ret}"
